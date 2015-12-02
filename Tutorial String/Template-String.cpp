3570669d2f04c13e3e0806f1a6a7d1b4091e7d63
hierony manurung <hierony.manurung@gdplabs.id>
2015-12-02 18:45:26 +0700

    Add Tutorial regarding String

---
+++ Tutorial String/Template-String.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+inline string IntToString(int a)
+{
+    char x[100];
+    sprintf(x,"%d",a);
+    string s = x;
+    return s;
+}
+
+inline int StringToInt(string a)
+{
+    char x[100];
+    int res;
+    strcpy(x,a.c_str());
+    sscanf(x,"%d",&res);
+    return res;
+}
+
+inline string GetString(void)
+{
+    char x[1000005];
+    scanf("%s",x);
+    string s = x;
+    return s;
+}
+
+
+int main()
+{
+    string kata = "makan";
+    reverse(kata.begin(), kata.end());
+
+    printf("%s\n",kata.c_str());
+
+    string angka = "12345";
+    int nilai = StringToInt(angka);
+
+    printf("Nilainya adalah : %d\n",nilai);
+
+    return 0;
+}
